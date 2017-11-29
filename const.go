f38951fa3b42c0d5f0456b325a570e221d59baaf
Eraac <kevin@labesse.me>
2017-11-29 21:48:28 +0100

    add const for some int

---
+++ const.go
@@ -0,0 +1,16 @@
+package gtfs
+
+// Const used in GTFS
+const (
+	RouteTypeTram      = 0
+	RouteTypeSubway    = 1
+	RouteTypeRail      = 2
+	RouteTypeBus       = 3
+	RouteTypeFerry     = 4
+	RouteTypeCableCar  = 5
+	RouteTypeGondola   = 6
+	RouteTypeFunicular = 7
+
+	ExceptionTypeAdded   = 1
+	ExceptionTypeRemoved = 2
+)
